b4863504aa29e3b9ec5e674421fb0f709afa65d6
Jieun Kang <ochoby@gmail.com>
2024-05-01 11:57:05 +0900

    style: "Container 컴포넌트"
    넓이: 768px
    정렬: 중앙
    배경색: 여백 - gray / 중앙 - white

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import BasicButton from './common/BasicButton';
+import Container from './common/Container';
 
 export default function Layout() {
   return (
     <>
-      <nav>nav 위치</nav>
-      <Outlet />
+      <Container>
+        <nav>nav 위치</nav>
+        <BasicButton text="button" />
+        <Outlet />
+      </Container>
     </>
   );
 }
