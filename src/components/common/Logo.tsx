1fc97e5a1c4dafb34272c9926058404cce62dbcc
Jieun Kang <ochoby@gmail.com>
2024-05-01 17:43:55 +0900

    style: "공통 컴포넌트: Logo, Navbar "

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+    // 색상이 적용되지 않는 상태
+    return (
+        <div className="font-bold tracking-tighter 
+        bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">
+            굳이데이!
+        </div>
+    )
+}
