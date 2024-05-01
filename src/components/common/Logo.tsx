a8f8b2003fb5125e9691bd52405a94aac6bdc6e3
Jieun Kang <ochoby@gmail.com>
2024-05-02 08:51:36 +0900

    style: "Logo 컴포넌트 시멘틱 마크업"
    h1 태그로 시멘틱 마크업

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Logo() {
     // 색상이 적용되지 않는 상태
     return (
-        <div className="font-bold tracking-tighter 
+        <h1 className="font-bold tracking-tighter 
         bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text text-transparent">
-            굳이데이!
-        </div>
+            <Link to='/' >굳이데이</Link>
+        </h1>
     )
 }
