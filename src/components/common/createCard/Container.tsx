d7dce168430a1134784c5d88611e142a821be164
Hansebin <sebinn052@gmail.com>
2024-05-02 14:33:59 +0900

    style "createCard page의 common 컴포넌트 - BasicButton, Container 스타일 수정 및 그라데이션 이슈 해결과 login page의 Loginbutton 컴포넌트"
    BasicButton의 그라데이션 이슈 해결: tailwindCSS 설정 파일에서 transparent를 명시적으로 지정
    Loginbutton: 로그인 페이지에서 사용되는 소셜 로그인 버튼 UI 구현

--- src/components/common/createCard/Container.tsx
+++ src/components/common/createCard/Container.tsx
@@ -5,7 +5,7 @@ interface IContainer {
 export default function Container({ children }: IContainer) {
   return (
     <div className='w-[400px] p-sm flex justify-center items-center bg-gradient-to-r from-secondary to-primary rounded-lg'>
-      <div className='w-full p-sm flex flex-col justify-center items-center gap-[30px] bg-white rounded-md'>
+      <div className='w-full p-sm flex flex-col justify-center items-center gap-4xl bg-white rounded-md'>
         {children}
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,40 @@ export default {
       lg: '976px',
       xl: '1440px'
     },
+    width: {
+      full: '100%',
+      screen: '100vw',
+      half: '50%',
+      base: '768px'
+    },
     colors: {
       primary: '#DB5895',
       secondary: '#EAA03C',
       black: '#404040',
       white: '#ffffff',
-      gray: '#f2f2f2'
+      gray: '#f2f2f2',
+      transparent: 'transparent'
     },
     spacing: {
+      // 1px ~ 9px
+      '1px': '1px',
+      '2px': '2px',
+      '3px': '3px',
+      '4px': '4px',
+      '5px': '5px',
+      '6px': '6px',
+      '7px': '7px',
+      '8px': '8px',
+      '9px': '9px',
+
+      // 10px ~ 자주 사용되는 단위로
       sm: '10px',
       md: '12px',
       lg: '14px',
       xl: '16px',
       '2xl': '18px',
-      '3xl': '20px'
+      '3xl': '20px',
+      '4xl': '30px'
     }
   },
   plugins: []
