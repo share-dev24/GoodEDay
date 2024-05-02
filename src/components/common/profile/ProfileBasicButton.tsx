3f9eda803734d01b016ebcfdd2a03af70df6f0bf
Hansebin <sebinn052@gmail.com>
2024-05-02 15:27:25 +0900

    style "ProfileBasicButton 컴포넌트"
    그라데이션 이슈 해결 및 일부 스타일 수정

--- src/components/common/profile/ProfileBasicButton.tsx
+++ src/components/common/profile/ProfileBasicButton.tsx
@@ -12,9 +12,17 @@ export default function ProfileBasicButton({
   return (
     <Link
       to={postCheck ? `${cardId}` : `create-post/${cardId}`}
-      className='block w-full p-[5px] bg-gradient-to-r from-secondary to-primary text-sm text-white font-semibold text-center rounded-full'
+      className='w-full p-2px flex justify-center items-center bg-gradient-to-r from-secondary to-primary text-sm font-semibold text-center rounded-full'
     >
-      {postCheck ? '실천 완료!' : '포스트 작성하기'}
+      <span
+        className={`w-full py-5px rounded-full ${postCheck ? 'bg-transparent' : 'bg-white'}`}
+      >
+        <span
+          className={`${postCheck ? 'text-white' : 'bg-clip-text text-transparent bg-gradient-to-r from-secondary to-primary'}`}
+        >
+          {postCheck ? '실천 완료!' : '포스트 작성하기'}
+        </span>
+      </span>
     </Link>
   );
 }
