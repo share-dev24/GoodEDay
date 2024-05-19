ba7d80ed682b6c2d30cbd2f2c3db91a1a4b0c641
Hansebin <sebinn052@gmail.com>
2024-05-19 17:11:05 +0900

    refactor: "컴포넌트 재사용을 위해 props 옵션 추가 및 조건부 처리"

--- src/components/createCard/CreateFormTitle.tsx
+++ src/components/createCard/CreateFormTitle.tsx
@@ -1,12 +1,22 @@
-export default function CreateFormTitle() {
+interface ICreateFormTitleProps {
+  page: string;
+  name?: string | null;
+}
+
+export default function CreateFormTitle({ page, name }: ICreateFormTitleProps) {
   return (
     <section className='w-full flex flex-col justify-center items-center gap-4xl text-center'>
       <h1 className='text-2xl text-black font-semibold'>
-        굳이데이 랜덤카드 생성하기 📃
+        {page === 'create'
+          ? '굳이데이 랜덤카드 생성하기 📃'
+          : `${name}님의 굳이데이 카드 🙂`}
       </h1>
       <h2 className='text-lg text-black font-medium'>
-        아래 옵션을 설정하여 <br></br>
-        나만의 굳이데이 램덤카드를 만들어보세요!
+        {page === 'create' ? '아래 옵션을 설정하여' : '완성된 굳이데이 카드를'}{' '}
+        <br></br>
+        {page === 'create'
+          ? '나만의 굳이데이 램덤카드를 만들어보세요!'
+          : '저장해서 실천하고 공유해보세요!'}
       </h2>
     </section>
   );
