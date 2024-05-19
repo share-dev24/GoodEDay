fd7060e987e1820f3953a10b207ba2c9dce5f6e8
Hansebin <sebinn052@gmail.com>
2024-05-19 17:10:23 +0900

    refactor : "굳이데이 랜덤카드 생성 컴포넌트 로직 수정 및 UI 변경, type 수정, props 수정"

--- src/routes/CreateCard.tsx
+++ src/routes/CreateCard.tsx
@@ -4,7 +4,7 @@ import CreateFormTitle from '../components/createCard/CreateFormTitle';
 export default function CreateCard() {
   return (
     <main className='w-[400px] mx-auto pt-[100px] flex flex-col justify-center items-center gap-[50px]'>
-      <CreateFormTitle />
+      <CreateFormTitle page='create' />
       <CreateForm />
     </main>
   );
