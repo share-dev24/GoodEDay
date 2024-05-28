41b0169f0e8d0301b13ecc5f6154f2898dc2c0ef
Hansebin <sebinn052@gmail.com>
2024-05-28 14:30:08 +0900

    docs: "코딩 컨벤션 충돌로 인한 불일치 해결"

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -2,52 +2,68 @@ import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import HeartIcon from './HeartIcon';
 import { Timestamp } from 'firebase/firestore';
-import getThemeKR from '../../modules/ThemeNameCompiling';
+import { getThemeKR } from '../../modules/ThemeNameCompiling';
 import getTimeSimple from '../../modules/TimeCompiler';
 import { useUserStore } from '../../stores/store';
 
 interface PostCardProps {
-    postId: string,
-    uid: string;
-    userName: string;
-    reviewDate: Timestamp;
-    theme: string;
-    imageUrl?: string;
-    content: string;
-    likeUserList: string[]
+  postId: string;
+  uid: string;
+  userName: string;
+  reviewDate: Timestamp;
+  theme: string;
+  imageUrl?: string;
+  content: string;
+  likeUserList: string[];
 }
 
-export default function PostCard({ imageUrl, userName, reviewDate, theme, content, postId, likeUserList }: PostCardProps) {
-    const { uid: storedUid } = useUserStore();
-    const [state, setState] = useState(false);
-    const location = useLocation();
+export default function PostCard({
+  imageUrl,
+  userName,
+  reviewDate,
+  theme,
+  content,
+  postId,
+  likeUserList
+}: PostCardProps) {
+  const { uid: storedUid } = useUserStore();
+  const [state, setState] = useState(false);
+  const location = useLocation();
 
-    useEffect(() => {
-        setState(likeUserList.includes(storedUid || ''));
-    }, [likeUserList]);
+  useEffect(() => {
+    setState(likeUserList.includes(storedUid || ''));
+  }, [likeUserList]);
 
-    // Determine the link based on the current location
-    const linkTo = location.pathname === '/' ? `/${postId}` : `/posts/${postId}`;
+  // Determine the link based on the current location
+  const linkTo = location.pathname === '/' ? `/${postId}` : `/posts/${postId}`;
 
-    return (
-        <div className='flex flex-col'>
-            <div className='flex flex-col w-[167px] h-[278px] text-ellipsis overflow-hidden'>
-                <div className='relative'>
-                    <img src={imageUrl} alt='후기' className='w-full h-[167px] rounded-[8px] object-cover' />
-                    <div className='absolute top-[140px] right-[8px]'>
-                        <HeartIcon postId={postId} state={state} />
-                    </div>
-                </div>
-                <div className='flex justify-between p-2px pt-4px'>
-                    <span className='font-bold'>{userName}</span>
-                    <span className='font-light text-sm flex items-center tracking-tight'>{getTimeSimple(reviewDate)}</span>
-                </div>
-                <div className='py-4px'>
-                    <span className='font-semibold inline-block'>{`[${getThemeKR(theme)}]`}</span>
-                    <span className='text-sm'>&nbsp;{content}</span>
-                </div>
-            </div>
-            <Link to={linkTo} className='text-left text-secondary text-sm'>자세히보기</Link>
+  return (
+    <div className='flex flex-col'>
+      <div className='flex flex-col w-[167px] h-[278px] text-ellipsis overflow-hidden'>
+        <div className='relative'>
+          <img
+            src={imageUrl}
+            alt='후기'
+            className='w-full h-[167px] rounded-[8px] object-cover'
+          />
+          <div className='absolute top-[140px] right-[8px]'>
+            <HeartIcon postId={postId} state={state} />
+          </div>
         </div>
-    );
+        <div className='flex justify-between p-2px pt-4px'>
+          <span className='font-bold'>{userName}</span>
+          <span className='font-light text-sm flex items-center tracking-tight'>
+            {getTimeSimple(reviewDate)}
+          </span>
+        </div>
+        <div className='py-4px'>
+          <span className='font-semibold inline-block'>{`[${getThemeKR(theme)}]`}</span>
+          <span className='text-sm'>&nbsp;{content}</span>
+        </div>
+      </div>
+      <Link to={linkTo} className='text-left text-secondary text-sm'>
+        자세히보기
+      </Link>
+    </div>
+  );
 }
